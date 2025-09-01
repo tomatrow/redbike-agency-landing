85c63bb33ac6ed1a889b2ec76c8b25d794148e79
AJ Caldwell <tomatrow@gmail.com>
2025-09-01 14:25:42 -0700

    Add post services section

---
+++ src/routes/home/PostServicesSection.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { FlyText } from "$lib"
+</script>
+
+<section>
+	<p>
+		<FlyText
+			text="Our multidisciplinary approach enables us to create complete and compelling brand experiences."
+		/>
+	</p>
+</section>
+
+<style>
+	section {
+		background: var(--accent);
+		color: var(--bg);
+		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		padding: 4rem 1rem;
+	}
+
+	p {
+		max-width: 1000px;
+		line-height: 1.5;
+		font-size: 2rem;
+
+		@media only screen and (min-width: 720px) {
+			font-size: 4rem;
+		}
+	}
+</style>
