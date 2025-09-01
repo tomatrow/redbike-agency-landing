c38d0f888195742255a0c2590c00f95208085e2d
AJ Caldwell <tomatrow@gmail.com>
2025-09-01 11:32:25 -0700

    Style the proof card a bit more

--- src/routes/home/ProofSection.svelte
+++ src/routes/home/ProofSection.svelte
@@ -1,38 +1,84 @@
 <script lang="ts">
 	import { Collapsible } from "melt/components"
-	import { slide } from "svelte/transition"
-	import { Cross } from "@lucide/svelte"
+	import { slide, fade } from "svelte/transition"
+	import { cubicOut } from "svelte/easing"
+	import { Plus } from "@lucide/svelte"
 
 	let name = "Hi-Fi Friends"
 	let items = ["Brand Identity", "Brand Logo", "Brand Strategy", "Web Design/Development"]
 </script>
 
-<article>
-	<img src="/images/readfriends.webp" alt={name} />
-	<Collapsible>
-		{#snippet children(collapsible)}
-			<button {...collapsible.trigger}>
-				<span>{name}</span>
-				<Cross />
-			</button>
+<section>
+	<article>
+		<img src="/images/readfriends.webp" alt={name} />
+		<Collapsible>
+			{#snippet children(collapsible)}
+				<button {...collapsible.trigger}>
+					<span>{name}</span>
+					<Plus class="icon {collapsible.open ? 'open' : 'closed'}" />
+				</button>
 
-			{#if collapsible.open}
-				<div {...collapsible.content} transition:slide>
-					<ul>
-						{#each items as item (item)}
-							<li><p>{item}</p></li>
-						{/each}
-					</ul>
-				</div>
-			{/if}
-		{/snippet}
-	</Collapsible>
-</article>
+				{#if collapsible.open}
+					<div {...collapsible.content} transition:slide>
+						<ul>
+							{#each items as item, index (item)}
+								<li transition:fade|global={{ delay: index * 150 + 100, easing: cubicOut }}>
+									<p>{item}</p>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				{/if}
+			{/snippet}
+		</Collapsible>
+	</article>
+</section>
 
 <style>
+	section {
+		padding: 1rem;
+	}
+
+	article {
+		display: flex;
+		flex-direction: column;
+	}
+
+	img {
+		aspect-ratio: 1;
+		min-height: 0;
+	}
+
 	button {
 		width: 100%;
 		display: flex;
 		justify-content: space-between;
+		color: var(--accent);
+		border-left: none;
+		border-right: none;
+		border-color: var(--text);
+		margin-top: 1rem;
+		color: var(--text);
+
+		background: none;
+		&:hover {
+			background: none;
+		}
+	}
+
+	ul {
+		padding-left: 1rem;
+	}
+
+	:global(.icon) {
+		transition: transform 0.3s cubic-bezier(0.215, 0.61, 0.355, 1);
+	}
+
+	:global(.icon.closed) {
+		transform: rotate(0deg);
+	}
+
+	:global(.icon.open) {
+		transform: rotate(45deg);
 	}
 </style>
