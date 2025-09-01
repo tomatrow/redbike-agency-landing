92967a5293cd9ffb2230c1fc1ba60e1335d6816c
AJ Caldwell <tomatrow@gmail.com>
2025-09-01 12:26:50 -0700

    Add initial post wrok section

---
+++ src/routes/home/PostProofSection.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { FlyText } from "$lib"
+</script>
+
+<section>
+	<p>
+		<FlyText
+			text="Every project possesses a unique purpose and beauty, requiring a customized collection of strategic and creative services to deliver results."
+		/>
+	</p>
+</section>
+
+<style>
+	section {
+		background: var(--accent);
+		color: var(--bg);
+		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		padding: 4rem 1rem;
+	}
+
+	p {
+		max-width: 1000px;
+		line-height: 1.5;
+		font-size: 2rem;
+
+		@media only screen and (min-width: 720px) {
+			font-size: 4rem;
+		}
+	}
+</style>
