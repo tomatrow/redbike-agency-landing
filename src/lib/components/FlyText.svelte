a226684eafe855539fd8d46fd095ae87f4588422
AJ Caldwell <tomatrow@gmail.com>
2025-09-01 12:44:27 -0700

    Fly in text with half the element intersects the viewport

--- src/lib/components/FlyText.svelte
+++ src/lib/components/FlyText.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { fly } from "svelte/transition"
 	import { cubicOut } from "svelte/easing"
+	import { useIntersectionObserver } from "runed"
 
 	let {
 		text,
@@ -9,20 +10,35 @@
 		y = 60
 	}: { y?: number; text: string; delayGap?: number; duration?: number } = $props()
 
-	const tokens = text.split(/(\s+)/)
+	let container = $state<HTMLElement>()
+	let showText = $state(false)
+	let oberver = useIntersectionObserver(
+		() => container,
+		(entries) => {
+			const ratio = entries[0]?.intersectionRatio ?? 0
+			if (ratio < 1 / 2) return
+
+			showText = true
+			oberver.stop()
+		}
+	)
+
+	let tokens = $derived(text.split(/(\s+)/))
 </script>
 
-<span class="words" aria-live="polite">
-	{#each tokens as token, i (i)}
-		{#if token.trim() === ""}
-			<!--  eslint-disable-next-line svelte/no-at-html-tags -->
-			{@html token}
-		{:else}
-			<span class="word" in:fly|global={{ y, duration, delay: i * delayGap, easing: cubicOut }}
-				>{token}</span
-			>
-		{/if}
-	{/each}
+<span bind:this={container} class="words" aria-live="polite">
+	{#if showText}
+		{#each tokens as token, i (i)}
+			{#if token.trim() === ""}
+				<!--  eslint-disable-next-line svelte/no-at-html-tags -->
+				{@html token}
+			{:else}
+				<span class="word" in:fly|global={{ y, duration, delay: i * delayGap, easing: cubicOut }}
+					>{token}</span
+				>
+			{/if}
+		{/each}
+	{/if}
 </span>
 
 <style>
