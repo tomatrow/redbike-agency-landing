1e38dca694c5e70f4be2fc647be7cf0eee810a41
AJ Caldwell <tomatrow@gmail.com>
2025-09-01 14:53:10 -0700

    Fille out contact section on home page

---
+++ src/routes/home/ContactSection.svelte
@@ -0,0 +1,68 @@
+<section id="contact">
+	<header>
+		<h2>Let’s Work Together</h2>
+		<p>
+			Red Bike Agency operates under the belief that businesses can perform more of a role within
+			our community than just providing services for profit. We want to use our skills, services,
+			and resources to support ideas, movements, and organizations that exist to do some good.
+			Sometimes this looks like us donating our resources or time, and other times it could be a
+			reduced fee or donation. Either way, it’s about doing our bit for better.
+		</p>
+		<a href="mailto:hello@redbike.agency">hello@redbike.agency</a>
+	</header>
+
+	<div class="calendy-container">
+		<!-- Calendly inline widget begin -->
+		<div class="calendly-inline-widget" data-url="https://calendly.com/redbikeagency"></div>
+		<script
+			type="text/javascript"
+			src="https://assets.calendly.com/assets/external/widget.js"
+			async
+		></script>
+		<!-- Calendly inline widget end -->
+	</div>
+</section>
+
+<style>
+	section {
+		padding: 4rem 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 4rem;
+		max-width: 1200px;
+		margin: auto;
+
+		@media (min-width: 720px) {
+			flex-direction: row;
+			gap: 2rem;
+
+			header,
+			.calendy-container {
+				flex: 1;
+			}
+		}
+	}
+
+	header {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+		p {
+			@media (min-width: 720px) {
+				max-width: 24rem;
+			}
+		}
+	}
+
+	.calendy-container {
+		.calendly-inline-widget {
+			min-width: 320px;
+			height: 500px;
+
+			@media (min-width: 720px) {
+				height: 100%;
+			}
+		}
+	}
+</style>
