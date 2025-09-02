4c9f21c7d6d13cf217ddace53d001de2552050c1
AJ Caldwell <tomatrow@gmail.com>
2025-09-01 21:18:33 -0700

    Add post hero section

---
+++ src/routes/about/PostHeroSection.svelte
@@ -0,0 +1,34 @@
+<section>
+	<h2>We help get your brand relevant.</h2>
+	<p>We do this by focusing on community and culture and using creativity to connect.</p>
+</section>
+
+<style>
+	section {
+		text-align: center;
+		padding: 6rem 1rem;
+		max-width: min(1200px, 80%);
+		margin: auto;
+	}
+
+	h2 {
+		font-weight: normal;
+		font-size: 1.5rem;
+		line-height: 1;
+		padding-bottom: 2rem;
+
+		@media (min-width: 720px) {
+			font-weight: 500;
+		}
+	}
+
+	p {
+		font-size: 2rem;
+		line-height: 1;
+		font-weight: 500;
+
+		@media (min-width: 720px) {
+			font-weight: bold;
+		}
+	}
+</style>
